129ea5e3ae9b9bae9184aa5ce11813879c83c90b
Vânia Almeida <vania.almeida28@hotmail.com>
2020-08-08 11:51:53 -0300

    configurando o nunjucks

--- src/server.js
+++ src/server.js
@@ -1,15 +1,26 @@
-const express = require("express");
+const express = require('express');
 const server = express();
+const nunjucks = require('nunjucks');
+
+const pageLanding = (req, res) => {
+  res.render('index.html');
+};
+
+const pageStudy = (req, res) => {
+  res.render('study.html');
+};
+const pageGiveClasses = (req, res) => {
+  res.render('give-classes.html');
+};
+
+nunjucks.configure('src/views', {
+  express: server,
+  noCache: true
+});
 
 server
-  .use(express.static("public"))
-  .get("/", (req, res) => {
-    res.sendFile(__dirname + "/views/index.html");
-})
-.get("/study", (req, res) => {
-    res.sendFile(__dirname + "/views/study.html");
-  })
-.get("/give-classes", (req, res) => {
-    res.sendFile(__dirname + "/views/give-classes.html");
-  })
+  .use(express.static('public'))
+  .get('/', pageLanding)
+  .get('/study', pageStudy)
+  .get('/give-classes', pageGiveClasses)
   .listen(5500);
